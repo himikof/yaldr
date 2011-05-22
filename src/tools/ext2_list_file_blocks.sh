d588e8ab01406db9f9c8de3c3223e7510f7d59a7
Kirill Elagin <kirelagin@gmail.com>
2011-05-22 19:25:23 +0400

    Add script which lists file blocks in ext2 fs

---
+++ src/tools/ext2_list_file_blocks.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+
+DEVICE="$1"
+FILENAME="$2"
+
+
+# Yeah, debugfs is that stupid
+unset PAGER
+
+
+BLOCK_SIZE=`debugfs "$DEVICE" -R 'stats' 2> /dev/null | grep "Block size:" | cut -d : -f 2 | sed 's/\s*//'`
+if [ -z "$BLOCK_SIZE" ]; then
+    exit -1
+fi
+FRAC=$(($BLOCK_SIZE / 512))
+
+BLOCKS_COUNT=`debugfs $DEVICE -R "stat \"$FILENAME\"" 2> /dev/null | grep "TOTAL:" | cut -d : -f 2 | sed 's/\s*//'`
+for i in `seq 0 $(($BLOCKS_COUNT-1))`; do
+    BLOCK_N=`debugfs $DEVICE -R "bmap \"$FILENAME\" $i" 2> /dev/null | sed 's/\s*//'`
+    if [ "$BLOCK_N" -eq 0 ]; then
+        break
+    fi
+
+    START=$(($BLOCK_N * $FRAC))
+    for k in `seq 0 $(($FRAC-1))`; do
+        echo -n $(($START + 512 * $k)) ''
+    done
+done
+
+echo
